5e6e180d2233a4c76cb78b7bfdc2aa472d4bbdbe
hkhoury <hkhoury@CHAFIK>
2023-02-22 12:31:29 +0200

    error when working in entity framework

---
+++ Asrfly.Data/SqlServer/DBContext.cs
@@ -0,0 +1,28 @@
+﻿using Asrfly.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asrfly.Data.SqlServer
+{
+    public class DBContext:DbContext
+    {
+        public DBContext()
+        {
+
+        }
+
+        //Set Connection String
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var sqlCon = @"Server=.\\SQLEXPRESS;Database=AsrflyDataBase;Trusted_Connection=True;"; // for test 
+            optionsBuilder.UseSqlServer(sqlCon);
+
+           
+    }
+        //Tables
+        public DbSet<Categories> Categories { get; set; }
+    }
+}
+    
